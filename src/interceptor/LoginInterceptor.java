2a0b4cec6c399ff66bd8509a311d81f6914f22c8
user <user@user-PC>
2016-10-31 15:31:35 +0900

    LoginInterceptor

---
+++ src/interceptor/LoginInterceptor.java
@@ -0,0 +1,23 @@
+package interceptor;
+
+import java.util.Map;
+
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+
+public class LoginInterceptor extends AbstractInterceptor {
+
+	@Override
+	public String intercept(ActionInvocation invocation) throws Exception {
+		Map<String, Object> session = invocation.getInvocationContext().getSession();
+		String loginId = (String)session.get("loginId");
+		
+		if(loginId == null){
+			return Action.LOGIN;
+		}
+		
+		return invocation.invoke();
+	}
+
+}
